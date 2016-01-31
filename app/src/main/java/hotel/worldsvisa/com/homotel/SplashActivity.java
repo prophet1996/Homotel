4e4a2b09859f6c31db0777740d0b9aa6b98109e5
prophet <ank26rock@gmail.com>
2016-01-31 13:58:35 +0530

    initial commit

---
+++ app/src/main/java/hotel/worldsvisa/com/homotel/SplashActivity.java
@@ -0,0 +1,13 @@
+package hotel.worldsvisa.com.homotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
